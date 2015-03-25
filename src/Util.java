e5138d9cf11aa06d4a6c3e02f00e71fc49a6b8a4
Aint Myat Noe <myatnoe.aint@gmail.com>
2015-03-25 14:33:12 -0400

    updated string sort to selection sort and submitted

--- src/Util.java
+++ src/Util.java
@@ -4,7 +4,23 @@ import java.util.Arrays;
 public class Util {
 	public static String sortString(String str){
 		char[] c = str.toCharArray();
-		Arrays.sort(c);
-		return new String(c);
+		//Arrays.sort(c);
+		if(str.length() > 1){
+			int min;
+			char tmp;
+			for(int i = 0; i < c.length ; i++){
+				min = i;
+				for( int j = i+1 ; j < c.length; j++){
+					if(c[j] < c[min]) min = j;
+				}
+				if(min != i){
+					tmp = c[min];
+					c[min] = c[i];
+					c[i] = tmp;
+				}
+			}
+			return new String(c);
+		}
+		return str;
 	}
 }
